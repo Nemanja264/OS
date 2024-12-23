b765e046f3ed1a8c556f636284774d743a70d096
Nemanja Ilic <155994048+Nemanja264@users.noreply.github.com>
2024-12-23 15:59:26 +0100

    Add files via upload

---
+++ blanketi/zad3/april_22/int.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#define KEY 10304
+
+struct msgbuf
+{
+    long mtype;
+    int mnum;
+};
